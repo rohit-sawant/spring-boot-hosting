b783cb11d0a76329d3e5863bcea26376a4fab779
rohit-sawant <sawant.rohit510@gmail.com>
2021-10-04 17:04:43 +0530

    first commit

---
+++ src/test/java/com/springwalk/sample/Testdeploy1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springwalk.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testdeploy1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
